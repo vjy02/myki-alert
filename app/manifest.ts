7a786874889fc42892b7f1722cc9d154c460522d
vjy02 <vyojoh@gmail.com>
2025-03-22 21:55:33 +1100

    fix manifest whoops

---
+++ app/manifest.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Inspector Alert",
+    short_name: "Inspector",
+    description: "Keep up to date with the latest activities from inspectors.",
+    start_url: "/",
+    display: "standalone",
+    icons: [
+      {
+        src: "/logo-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/logo-512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  }
+}
